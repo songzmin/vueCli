0b3bb8dea1e7082e31bc199aa728820ea594b163
songzmin <songzmin@163.com>
2019-06-18 23:09:15 +0800

    有关vue的async，await的内容

---
+++ Vue_async/code/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <HelloWorld :message="msg"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+
+export default {
+  name: "app",
+  components: {
+    HelloWorld
+  },
+  data() {
+    return {
+      msg: ""
+    };
+  },
+  async created() {
+    this.msg = await "songzmin";
+    console.log("wo shi shou ye");
+  }
+};
+</script>
+
+<style lang="less">
+</style>
---
+++ Vue_async/code/components/HelloWorld.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>{{data}}</div>
+</template>
+
+<script>
+export default {
+  props: ["message"],
+  data() {
+    return {
+      data: ""
+    };
+  },
+  created() {
+    console.log(this.message);
+  },
+  watch: {
+    message(val) {
+      this.data = val;
+    }
+  }
+};
+</script>
+<style scoped lang="less">
+</style>
